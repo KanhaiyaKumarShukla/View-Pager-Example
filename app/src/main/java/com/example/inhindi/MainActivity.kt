d04ea66e61f0606a42c1c9e8a9a6a78024ecd71c
KanhaiyaKumarShukla <kanhaiyashukla0.01@gmail.com>
2023-11-28 04:13:09 +0530

    another commit

--- app/src/main/java/com/example/inhindi/MainActivity.kt
+++ app/src/main/java/com/example/inhindi/MainActivity.kt
@@ -1,34 +1,18 @@
 package com.example.inhindi
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tvAnimal: TextView=findViewById(R.id.tvAnimal);
-        val tvCity: TextView=findViewById(R.id.tvCity)
-        val tvFruit: TextView=findViewById(R.id.tvFruit)
-        val tvFlower: TextView=findViewById(R.id.tvFlower)
-        tvAnimal.setOnClickListener{
-            val intent=Intent(this, animal::class.java)
-            startActivity(intent)
-        }
-        tvFruit.setOnClickListener{
-            val intent=Intent(this, fruit::class.java)
-            startActivity(intent)
-        }
-        tvCity.setOnClickListener {
-            val intent=Intent(this, city::class.java)
-            startActivity(intent)
-        }
-        tvFlower.setOnClickListener{
-            val intent=Intent(this, flower::class.java)
-            startActivity(intent)
-        }
+        val viewPager=findViewById<ViewPager>(R.id.viewpager)
+        val adapter= CategoryAdapter(this,supportFragmentManager)
+        viewPager.adapter=adapter
+        val tabLayout=findViewById<TabLayout>(R.id.tabs)
+        tabLayout.setupWithViewPager(viewPager)
     }
 }
